d26ec4ef3e415baed95926cafe4ed44e1e90fabc
updato111 <69454933+updato111@users.noreply.github.com>
2023-12-23 19:48:31 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=1619182628
-    API_HASH="ksjs"
-    TOKEN="jaksks"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
-    START_IMG="jdjsjw"
-    BOT_ID=1013
-    BOT_USERNAME="djsjw"
-    BOT_NAME="BAN ALL"
+    API_ID=24638763
+    API_HASH="e02a72843ababb75de3c4f3aa2ce8d75"
+    TOKEN="6609962299:AAFjKbnHk4ZLOgIGQl7b77xXztMGmUPMOLc"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5699636836").split(" "))
+    START_IMG="BAF39SsAh1NI60jY7oJqaHK7bnSB9n_UyWUifw7qvRaf_tbPqVVL64geaNOYfmnNz_nUxo1c9OOgsy9Sl11kAdydt2uUkyUNxLdlMlc9xioi7i9YObgoX-b8OxugDpzJaydZ9MGdB-qf7qk5gHddpIZc4vLeLS0NplPR2U65fIzyhCm9oHJvi75fH7AClwIVLaZr7TLQ_kk071HULWQEC0KHHkiodSoD_0DmadE0jR8aPMqgvtRmB-CJLEuc5EX9aqb7q-QnkaPsF0IhPVSKPz7j4uYgdJAxD3pAitx6n4_qAv-ho4DLwnmLkTzh119O4FYzcrTRQAduCaP6Jd8NyNXs2H_d3QAAAAFTuY5kAA"
+    BOT_ID=6609962299
+    BOT_USERNAME="VieraRemoverbot"
+    BOT_NAME="Viera Member Remover"
